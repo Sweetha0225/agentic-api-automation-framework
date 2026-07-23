08e18fcfe2b1b6ea4d500667eded4a69a83f50d7
Sweetha0225 <sweetha.s@ascendion.com>
2026-07-23 17:47:58 +0530

    Chnages in The Plugins endpoints

--- src/api/plugins/PluginsEndpoints.js
+++ src/api/plugins/PluginsEndpoints.js
@@ -2,10 +2,10 @@ const PLUGINS_ENDPOINTS = {
     GET_ALL: '/search/unified/search/entity?types=plugins&pageNumber=0&pageSize=15',
     DOWNLOAD_VSCODE: '/admin/component?componentName=VsCode&email=sweetha.s@ascendion.com',
     DOWNLOAD_VISUAL_STUDIO: '/admin/component?componentName=Visual%20Studio&email=sweetha.s@ascendion.com',
-    DOWNLOAD_ANDROID_STUDIO: '/admin/component?componentName=%20Android%20Studio&email=sweetha.s@ascendion.com',
+    DOWNLOAD_ANDROID_STUDIO: '/admin/component?componentName=Android%20Studio&email=sweetha.s@ascendion.com',
     DOWNLOAD_ECLIPSE: '/admin/component?componentName=Eclipse&email=sweetha.s@ascendion.com',
-    DOWNLOAD_INTELLIJ: '/admin/component?componentName=%20Intellij&email=sweetha.s@ascendion.com',
-    DOWNLOAD_PYCHARM: '/admin/component?componentName=%20phyCharm&email=sweetha.s@ascendion.com'
+    DOWNLOAD_INTELLIJ: '/admin/component?componentName=Intellij&email=sweetha.s@ascendion.com',
+    DOWNLOAD_PYCHARM: '/admin/component?componentName=PyCharm&email=sweetha.s@ascendion.com'
 };
 
 module.exports = PLUGINS_ENDPOINTS;
